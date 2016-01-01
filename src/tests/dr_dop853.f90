72ca0609008de6651d6c798da0a9874198f776b6
Jacob Williams <jacobwilliams@users.noreply.github.com>
2016-01-01 00:09:40 -0600

    refactoring.

--- src/tests/dr_dop853.f90
+++ src/tests/dr_dop853.f90
@@ -5,20 +5,29 @@
 program dr_dop853
     use dop853_module
     use dop853_constants
+    use iso_fortran_env, only: output_unit
 
       implicit none
 
+      type(dop853_class) :: prop
+
+      integer :: i
+      integer,parameter :: n = 2  !dimension of the system
+      integer,dimension(n),parameter :: icomp = [(i,i=1,n)]
+
       real(wp) :: atol , rpar , rtol , tol , work , x ,     &
            & xend , y
-      integer i , idid , iout , ipar , itol , iwork , j , liwork ,      &
-            & lwork , n , ndgl , nrd
+      integer idid , iout , ipar , itol , iwork , j , liwork ,      &
+            & lwork , ndgl , nrd
       parameter (ndgl=2,nrd=2)
       parameter (lwork=11*ndgl+8*nrd+21,liwork=nrd+21)
       dimension y(ndgl) , work(lwork) , iwork(liwork)
       dimension rtol(1) , atol(1) , rpar(1) , ipar(1)
 
+      integer :: nfcn,nstep,naccpt,nrejct
+      logical :: status_ok
+
 ! --- dimension of the system
-      n = 2
       rpar = 1.0d-3
 ! --- output routine (and dense output) is used during integration
       iout = 3
@@ -33,24 +42,19 @@ program dr_dop853
       itol = 0
       rtol = tol
       atol = tol
-! --- default values for parameters
-      do i = 1 , 10
-         iwork(i) = 0
-         work(i) = 0.d0
-      enddo
-      iwork(5) = n
-      iwork(4) = 1
-! --- call of the subroutine dopri8
-      call dop853(n,fvpol,x,y,xend,rtol,atol,itol,solout,iout,work,     &
-                & lwork,iwork,liwork,idid)
+
+      call prop%initialize(ICOMP=icomp,NSTIFF=1,STATUS_OK=status_ok)
+      call prop%integrate(n,fvpol,x,y,xend,rtol,atol,solout,iout,idid)
+      call prop%info(nfcn,nstep,naccpt,nrejct)
+
 ! --- print final solution
       write (6,99001) x , y(1) , y(2)
 99001 format (1x,'x =',f5.2,'    y =',2e18.10)
 ! --- print statistics
       write (6,99002) tol
 99002 format ('       tol=',d8.2)
-      write (6,99003) (iwork(j),j=17,20)
-99003 format (' fcn=',i5,' step=',i4,' accpt=',i4,' rejct=',i3)
+      write (output_unit,'(A,I5,A,I4,A,I4,A,I3)') &
+              ' fcn=',nfcn,' step=',nstep,' accpt=',naccpt,' rejct=',nrejct
 
       contains
 
@@ -60,17 +64,28 @@ program dr_dop853
 
       implicit none
 
-      real(wp) :: con , x , xold , xout , y
-      integer icomp , irtrn , n , nd , nr
-      dimension y(n) , con(8*nd) , icomp(nd)
+      integer,intent(in)                  :: nr
+      real(wp),intent(in)                 :: xold
+      real(wp),intent(in)                 :: x
+      integer,intent(in)                  :: n
+      integer,intent(in)                  :: nd
+      real(wp),dimension(n),intent(in)    :: y
+      real(wp),dimension(8*nd),intent(in) :: con
+      integer,dimension(nd),intent(in)    :: icomp
+      integer,intent(inout)               :: irtrn
+      real(wp),intent(out)                :: xout
+
+     ! real(wp) :: con , x , xold , xout , y
+     ! integer icomp , irtrn , n , nd , nr
+     ! dimension y(n) , con(8*nd) , icomp(nd)
 
       if ( nr==1 ) then
          write (6,99001) x , y(1) , y(2) , nr - 1
          xout = 0.1d0
       else
  50      if ( x>=xout ) then
-            write (6,99001) xout , contd8(1,xout,con,icomp,nd) ,        &
-                          & contd8(2,xout,con,icomp,nd) , nr - 1
+            write (6,99001) xout , prop%contd8(1,xout,con,icomp,nd) ,        &
+                          & prop%contd8(2,xout,con,icomp,nd) , nr - 1
             xout = xout + 0.1d0
             goto 50
          endif
